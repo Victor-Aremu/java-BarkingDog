96261f3f275149552bb4e0e144e9fc1318bfa517
Victor-Aremu <77357316+Victor-Aremu@users.noreply.github.com>
2021-01-17 16:28:38 +0000

    small changes made

--- src/BarkingDog.java
+++ src/BarkingDog.java
@@ -1,10 +1,4 @@
 public class BarkingDog {
-    public static void main(String[] args) {
-        System.out.println(shouldWakeUp(true, 1));
-        System.out.println(shouldWakeUp(false, 2));
-        System.out.println(shouldWakeUp(true, 8));
-        System.out.println(shouldWakeUp(true, -1));
-    }
     public static boolean shouldWakeUp(boolean barking, int hourOfDay) {
         if (hourOfDay < 0 || hourOfDay > 23) {
             return false;
@@ -15,3 +9,4 @@ public class BarkingDog {
         }
     }
 }
+//Victor.Aremu
